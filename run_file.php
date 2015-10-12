ed97d8424ded3028211fee11bacfed1f2ecbc1f8
arguv <gurgen.aramian@gmail.com>
2015-10-12 23:49:49 +0400

    added put and get methods and revised all

--- run_file.php
+++ run_file.php
@@ -2,61 +2,73 @@
 /*
 *	Easy example to run PDAdapter
 */
+//=========== you can get id from URL (it's not required)============
+	$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+	$query_str = parse_url($actual_link, PHP_URL_QUERY);
+
+	parse_str($query_str, $query_params);
+	
+	$people_id = $query_params['id'];
+//=========================================================
+
 require_once('PDAdapter.php');
 
-//$post_back['lead[w2lid]'] = "xxxxxxxxxxxxxx";
+$resources = null;  // people, notes, deals ...
+$conditions = null;
+$page = null;		// 1,2,3,4,5 ...
+$_postback = null;
 
-$post_back['lead[first_name]'] = "Test";
-$post_back['lead[last_name]'] = "Test";
-$post_back['lead[email]'] = "test@test.local";
-$post_back['lead[phone]'] ="3333-5555";
-$post_back['lead[type]'] ="Lead";
-$post_back['lead[lead_source_id]'] ="645724"; //Web to Lead 
- 
-//$post_back['lead[company_name]'] = "Test Company";
+$api_key = 'xzxzxzxzxzxzxzxzxzxz';	// Enter your API key
+			
+$pda = new PDAdapter($api_key);
+//=========================================================
+//========== some details for using 'people' ==============
 
-//$post_back['lead[custom_fields[custom_label_1009214]'] = "custom_label_first_section";
-//$post_back['lead[custom_fields[custom_label_1018289]'] = "828733";
-//$post_back['lead[custom_fields[custom_label_1038041]'] = "877127";
-//$post_back['lead[custom_fields[custom_label_1038048]'] = "877130";
-//$post_back['lead[custom_fields[custom_label_1038049]'] = "20150620";
-//$post_back['lead[custom_fields[custom_label_1041539]'] = "885395";
-//$post_back['lead[custom_fields[custom_label_1071910]'] = "967057";
+	//$_postback['person[w2lid]'] 	= "xxxxxxxxxxxxxx"; // here must be yours
+	
+	$_postback['person[first_name]'] 		= "Test";
+    $_postback['person[last_name]']	 		= "Test";
+    $_postback['person[email]'] 			= "test@test.com";
+	$_postback['person[phone]'] 			= "3333-5555";
+	$_postback['person[type]'] 				= "Lead";
+	$_postback['person[lead_source_id]'] 	= "645724";	//Web to Lead
+	$_postback['person[company_name]'] 	= "Test Company";
+	$_postback['person[custom_fields[custom_label_1009214]]'] = "custom_label_first_section";
+	$_postback['person[custom_fields[custom_label_1018289]]'] = "828733";
+	$_postback['person[custom_fields[custom_label_1038041]]'] = "877127";
+	$_postback['person[custom_fields[custom_label_1038048]]'] = "877130";
+	$_postback['person[custom_fields[custom_label_1038049]]'] = "20150620";
+	$_postback['person[custom_fields[custom_label_1041539]]'] = "885395";
+	$_postback['person[custom_fields[custom_label_1071910]]'] = "967057";
+	
+//=========== some details for using 'notes' ==============
 
-	$note['note[person_id]'] = '1039959805';
+    $note['note[person_id]'] = '1039959805';
 	$note['note[user_id]'] = '19095';
 	$note['note[note_category_id]'] = '144576';
 	$note['note[content]'] = 'This is TEST comment';
-
-//============================================
-	//$_postback['person[w2lid]'] 	= $post_back['lead[w2lid]'];
 	
-	$_postback['person[first_name]'] 		= $post_back['lead[first_name]'];
-    $_postback['person[last_name]']	 		= $post_back['lead[last_name]'];
-    $_postback['person[email]'] 			= $post_back['lead[email]'];
-	$_postback['person[type]'] 				= $post_back['lead[type]'];
-	$_postback['person[phone]'] 			= $post_back['lead[phone]'];
-	$_postback['person[lead_source_id]'] 	= $post_back['lead[lead_source_id]'];
-	
-	//$_postback['person[company_name]'] 	= $post_back['lead[company_name]'];
+//================= for creating ===========================
 
-	//$_postback['person[custom_fields[custom_label_1009214]]'] = $post_back['lead[custom_fields[custom_label_1009214]'];
-	//$_postback['person[custom_fields[custom_label_1018289]]'] = $post_back['lead[custom_fields[custom_label_1018289]'];
-	//$_postback['person[custom_fields[custom_label_1038041]]'] = $post_back['lead[custom_fields[custom_label_1038041]'];
-	//$_postback['person[custom_fields[custom_label_1038048]]'] = $post_back['lead[custom_fields[custom_label_1038048]'];
-	//$_postback['person[custom_fields[custom_label_1038049]]'] = $post_back['lead[custom_fields[custom_label_1038049]'];
-	//$_postback['person[custom_fields[custom_label_1041539]]'] = $post_back['lead[custom_fields[custom_label_1041539]'];
-	//$_postback['person[custom_fields[custom_label_1071910]]'] = $post_back['lead[custom_fields[custom_label_1071910]'];
-    
+$resources = "people";
 
-$resources = "people";  //people or notes ...
-$conditions = null;
-$page = null;
+$pda->setMethod('post'); //create
 
-$api_key = 'xzxzxzxzxzxzxzx';
+$result = $pda->doRequest($resources, $conditions, $page, $_postback);  // $_postback or $note ...
 
-$pda = new PDAdapter($api_key);
+//================= for getting ============================
 
-$pda->setMethod('post');
+$resources = "people/".$people_id;	// $people_id -> some person id (for instance 12854350) that you want to get
 
-$result = $pda->doRequest($resources, $conditions, $page, $_postback);  //$_postback or $note ...
+$pda->setMethod('get');	//get
+
+$result = $pda->doRequest($resources, $conditions, $page, $_postback);	// $_postback or $note ...
+
+//================= for updating ============================
+
+$resources = "people/".$people_id;	// $people_id -> some person id (for instance 12854350) that you want to update
+
+$pda->setMethod('put');	//update
+
+$result = $pda->doRequest($resources, $conditions, $page, $_postback);	// $_postback or $note ...
